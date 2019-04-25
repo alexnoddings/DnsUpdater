4f7b500a519f80e95e95394f28aead59a29b8ef5
LiamJBell <liamjbell97@gmail.com>
2019-04-25 17:03:24 +0100

    Add project files.

---
+++ Application/Program.cs
@@ -0,0 +1,20 @@
+﻿using Core;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    class Program
+    {
+        private static IDnsUpdaterService _updater;
+        static void Main(string[] args)
+        {
+            _updater = new DnsUpdaterService();
+            _updater.Start();
+        }
+    }
+}
---
+++ Core/Interfaces/IDnsUpdaterService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Core.Interfaces
+{
+    public interface IDnsUpdaterService
+    {
+        void Start();
+        void Stop();
+        void OnElapsedTime(object source, ElapsedEventArgs e);
+        string GetWebResponse(string url);
+        string GetIp();
+        string UpdateIp(string newIp);
+    }
+}
