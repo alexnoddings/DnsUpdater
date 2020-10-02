fb3c55bc2aa2308aa13e6a0b78d171ca6fcaa8b8
Alex Noddings <alexnoddings@users.noreply.github.com>
2020-10-02 14:18:38 +0100

    Add Ipify IP resolver.

---
+++ IpResolvers.Ipify/IpifyResolverService.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Core.Services;
+using Microsoft.Extensions.Logging;
+
+namespace IpResolvers.Ipify
+{
+    internal class IpifyResolverService : IIpAddressResolver
+    {
+        private const string ApiEndpoint = "https://api.ipify.org";
+
+        private ILogger<IpifyResolverService> Logger { get; }
+
+        private HttpClient HttpClient { get; } = new HttpClient();
+
+        public IpifyResolverService(ILogger<IpifyResolverService> logger)
+        {
+            Logger = logger;
+        }
+
+        public async Task<IPAddress?> GetCurrentIpAddressAsync()
+        {
+            HttpResponseMessage response = await HttpClient.GetAsync(ApiEndpoint);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                HttpStatusCode status = response.StatusCode;
+                string errorBody = await response.Content.ReadAsStringAsync();
+                Logger.LogError($"Failed up fetch IP: {status} {errorBody}");
+                return null;
+            }
+
+            string body = await response.Content.ReadAsStringAsync();
+            if (!IPAddress.TryParse(body, out IPAddress? ipAddress))
+            {
+                Logger.LogError($"Could not parse response: {body}");
+                return null;
+            }
+
+            return ipAddress;
+        }
+    }
+}
---
+++ IpResolvers.Ipify/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IpResolvers.Ipify
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddIpifyResolverService(this IServiceCollection services)
+        {
+            services.AddScoped<IIpAddressResolver, IpifyResolverService>();
+
+            return services;
+        }
+    }
+}
